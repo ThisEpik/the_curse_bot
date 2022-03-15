ac168eca4f71fc43b9280c74717ac505b88510e2
Andrey Nebogin <this.sky.work@gmail.com>
2022-03-15 13:46:17 +0600

    уменьшенно кол-во слов в ответах

--- bin/server.dart
+++ bin/server.dart
@@ -12,6 +12,7 @@ Future<void> main() async {
   String? username = (await Telegram(token).getMe()).username;
   TeleDart teledart = TeleDart(token, Event(username!));
 
+  print('object');
   teledart.start();
   teledart.onCommand().listen((event) {
     event.reply(
@@ -37,7 +38,7 @@ Future<String> getAnswer(String inputString) async {
   String url = 'https://pelevin.gpt.dobro.ai/generate/';
   try {
     Response response = await http.post(Uri.parse(url),
-        body: jsonEncode({"prompt": newWord, "length": 50}));
+        body: jsonEncode({"prompt": newWord, "length": 30}));
     dynamic responseBody = await json.decode(utf8.decode(response.bodyBytes));
     for (var el in responseBody['replies']) {
       newAnswer += await el;
